2850b082d5099f28222869b5e5f8bc954bca3897
jcheung9941 <75763997+jcheung9941@users.noreply.github.com>
2024-12-31 03:12:31 -0500

    add functionality to display singles

--- will it fit.js
+++ will it fit.js
@@ -1,18 +1,19 @@
 const shipList = arr
-// getShipDetails() //need to modify to get correct url- ?&{type}&id=&{uid.split(':')[1]}
+// const shipList =[]
+// getShipDetails() //disable =arr if this is active
 const cargo = { weight: 0, volume: 0, ships: {} }
 
 let isLoaded = true //default false. probably state. if done at all.
 const loading = document.getElementById('loading')
 if (isLoaded) {
     loading.parentNode.removeChild(loading)
-    while(document.querySelector('.hidden')) {
+    while (document.querySelector('.hidden')) {
         document.querySelector('.hidden').classList.remove('hidden')
     }
-    
+
     const select = document.createElement('select')
     select.oninput = updateSelected //update selected entity stats
-    shipList.map((element) => { //populate dropdown
+    shipList.forEach((element) => { //populate dropdown
         const option = document.createElement('option')
         option.innerText = element.shipName
         select.appendChild(option)
@@ -25,10 +26,6 @@ if (isLoaded) {
     document.querySelector('#total-weight').innerText = (findShip().weight * quantity).toLocaleString()
     document.querySelector('#total-volume').innerText = (findShip().volume * quantity).toLocaleString()
 }
-
-
-
-
 
 function findShip() {
     return shipList.find((ship) => { return ship.shipName === document.querySelector('select').value })
@@ -46,10 +43,17 @@ function updateSelected() {
 }
 
 function addCargo() {
+    // todo:
+    // add ability to combine like entities
+    // add ability to remove entities
+    // check if can figure out how to see performance cost of redrawing entire list vs replacing nodes
+    while (document.querySelector(`#cargoDiv p`)) {
+        document.querySelector(`#cargoDiv p`).parentNode.removeChild(document.querySelector(`#cargoDiv p`))
+    }
     const ship = shipList.find((ship) => { return ship.shipName === document.querySelector('select').value })
-    const cargoDivHeaders = ['shipName', 'quantity', 'weight', 'volume']
     let quantity = +document.querySelector('#quantity-input').value
-    cargoDivHeaders.map((element) => {
+    const cargoDivHeaders = ['shipName', 'quantity', 'weight', 'volume']
+    cargoDivHeaders.forEach((element) => {
         const p = document.createElement('p')
         if (element != 'quantity') {
             p.innerText = ship[element].toLocaleString()
@@ -63,6 +67,32 @@ function addCargo() {
     if (cargo.ships[ship.shipName]) { quantity += cargo.ships[ship.shipName].quantity }
     cargo.ships[ship.shipName] = { quantity: quantity, stats: ship }
     document.querySelector('#cargoDiv h2').innerText = `Cargo (${cargo.weight.toLocaleString()}T / ${cargo.volume.toLocaleString()}m³ )`
+
+    let single = shipList.filter((ship) => {
+        if (ship.weightCapacity >= cargo.weight && ship.volumeCapacity >= cargo.volume && ship.isDocking) { return true } else { return false }
+    })
+    console.log(single) //
+    addNewLine(single, 'single')
+
+
+}
+
+function addNewLine(ships, id) {
+    while (document.querySelector(`#${id} p`)) { // todo: see if there's a better way to do this
+        document.querySelector(`#${id} p`).parentNode.removeChild(document.querySelector(`#${id} p`))
+    }
+    const divHeaders = ['shipName', 'hyper', 'qty', 'party']
+    ships.forEach((ship) => {
+        divHeaders.forEach((element) => {
+            const p = document.createElement('p')
+            if (element != 'qty') {
+                p.innerText = ship[element].toLocaleString()
+            } else {
+                p.innerText = Math.floor(Math.ceil(cargo.weight / ship.weightCapacity, cargo.volume / ship.volumeCapacity))
+            }
+            document.querySelector(`#${id}`).appendChild(p)
+        })
+    })
 }
 
 function sleep(ms) {
@@ -84,10 +114,10 @@ async function getShipUid() {
             const data = await response.json()
 
             resAttributes = { ...data.swcapi.shiptypes.attributes }
-            loading.innerText = `Loading... identified ${start}/${total} ships`
+            loading.innerText = `Loading... identified ${resAttributes.start}/${resAttributes.total} ships`
             resAttributes.start = +resAttributes.start + +resAttributes.count
             data.swcapi.shiptypes.shiptype.forEach(element => {
-                shipList.push({ uid: element.attributes.uid, href: element.attributes.href, shipName: element.value, })
+                shipList.push({ uid: element.attributes.uid, shipName: element.value, })
             });
         }
     } catch (error) {
@@ -112,11 +142,12 @@ async function getShipDetails() {
                 data = data.swcapi.shiptype
                 shipList[i] = {
                     ...element,
+                    href: `https://www.swcombine.com/rules/?${data.class.attributes.value}&ID=${element.uid.split(':')[1]}`,
                     hyper: data.speed.hyperspace,
                     weight: data.weight.value,
                     volume: data.volume.value,
                     weightCapacity: data.weightcapacity.value,
-                    volumeCapacity: data.volumecapacity.vaue,
+                    volumeCapacity: data.volumecapacity.value,
                     passengers: data.maxpassengers,
                     party: data.slotsize,
                     isHangar: data.hangarbay === "yes" ? true : false,
@@ -124,7 +155,7 @@ async function getShipDetails() {
                     images: data.images,
                 }
 
-                if (i + 1 === shipList.length()) { isLoaded = true }
+                if (i + 1 === shipList.length) { isLoaded = true } //eh. doesn't actually work, probably need to convert if(isLoaded) to a function
                 console.log("counter:", i, "details:", shipList)
             })
 
